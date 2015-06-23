fd1b04884f1b635df21b9924cb5962ff8eb303ea
STAM <stam@epicm.org>
2015-06-23 23:09:41 +0300

    full cleanup

--- SRC/gSDK_Launcher/Core/Globals.cs
+++
@@ -1,8 +0,0 @@
-﻿using gSDK_Launcher.UI;
-
-namespace gSDK_Launcher.Core {
-    internal static class Globals {
-        public static Config Config { get; set; }
-        public static AbyrvalgTranslator Translator { get; set; }
-    }
-}
